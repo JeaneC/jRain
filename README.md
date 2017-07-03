ef007e15b7bcee4ff6fef212a1620b865b242d70
Jeane Carlos <volbohel@gmail.com>
2017-07-02 23:01:48 -0400

    V 0.1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a local weather app coded in Swift. The app was a project assigned by the Udemy devslopes course.
+
+V 0.0 - First commit
+V 0.1 - UI is basically designed
---
+++ jRain/WeatherViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherViewController.swift
+//  jRain
+//
+//  Created by Jeane Carlos on 7/2/17.
+//  Copyright © 2017 Jeane Carlos. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 80 //Unfortunately this is the manual way to do it, since it's bugged
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+
+
+}
+
