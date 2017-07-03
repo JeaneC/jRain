4c2301a6004f837a81d219936afec696e6c444d9
Jeane Carlos <volbohel@gmail.com>
2017-07-02 21:42:01 -0400

    First Commit

---
+++ Pods/Target Support Files/Pods-jRain/Pods-jRain-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_jRain : NSObject
+@end
+@implementation PodsDummy_Pods_jRain
+@end
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a local weather app coded in Swift. The app was a project assigned by the Udemy devslopes course.
